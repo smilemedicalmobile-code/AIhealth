eade31ac727bd45e19e7939eadd621e45ab35128
chaedongjoo <ehdwnl0604@gmail.com>
2025-10-28 10:06:22 +0900

    Initial Commit

---
+++ AIhealth/AIhealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIhealthApp.swift
+//  AIhealth
+//
+//  Created by 채동주 on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AIhealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIhealthTests/AIhealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIhealthTests.swift
+//  AIhealthTests
+//
+//  Created by 채동주 on 10/28/25.
+//
+
+import Testing
+@testable import AIhealth
+
+struct AIhealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
